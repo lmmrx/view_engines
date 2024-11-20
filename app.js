516fe5b3db7c63319d946f5345e9d72c1a74351c
Rj Lemmor Maglaqui <rjlemmormaglaqui@gmail.com>
2024-11-19 19:03:47 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 8000;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/home", (req, res)=> {
+    const name = "rj";
+    res.render("index", {name});
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Connected to port ${PORT}`);
+});
